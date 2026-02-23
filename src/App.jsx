6dfac0f12c40b2e85aa82ec88997d6eb8ee85ec5
Rohit Jain <Jain74159@gmail.com>
2026-02-24 00:09:16 +0530

    Force re-track all files and fix case issues

--- src/App.jsx
+++ src/App.jsx
@@ -13,21 +13,20 @@ import Blog from "./pages/Blog";
 
 /* User */
 import Jobs from "./pages/Jobs";
-import JobDetails from "./pages/Jobdetails";
-import ApplyPage from "./pages/Applypage";
+import JobDetails from "./pages/JobDetails";          // correct
+import ApplyPage from "./pages/Applypage";            // small p
 import Profile from "./pages/Profile";
-import MyApplications from "./pages/Myapplications";
-import UserDashboard from "./pages/Userdashboard";
+import MyApplications from "./pages/Myapplications";  // small a
+import UserDashboard from "./pages/Userdashboard";    // small d
 
 /* Admin */
-import AdminDashboard from "./pages/Admindashboard";
-import ManageJobs from "./pages/Managejobs";
-import AdminApplications from "./pages/Adminapplications";
-import AdminUsers from "./pages/Adminusers";
-import AddJob from "./pages/Addjob";
+import AdminDashboard from "./pages/Admindashboard";        // small d
+import ManageJobs from "./pages/Managejobs";                // small j
+import AdminApplications from "./pages/Adminapplications";  // small a
+import AdminUsers from "./pages/Adminuser";                 // singular + small u
+import AddJob from "./pages/Addjob";                        // small j
 
 function App() {
-
   const user = JSON.parse(localStorage.getItem("user") || "null");
 
   return (
